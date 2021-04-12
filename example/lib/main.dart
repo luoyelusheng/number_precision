c66853f1f60b4870d712be348fd8e0b1cf8f5961
luoyelusheng <luoyelusheng@gmail.com>
2021-04-12 10:01:59 +0800

    docs: completed the demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,14 +8,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print(0.105.toStringAsFixed(2));
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('${NP.minus(8893568.31249, -7.2550)}'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('0.1 + 0.3 = ${NP.plus(0.1, 0.3)}'),
+              Text('0.1 - 0.3 = ${NP.minus(0.1, 0.3)}'),
+              Text('0.1 * 0.3 = ${NP.times(0.1, 0.3)}'),
+              Text('0.1 / 0.3 = ${NP.round(NP.divide(0.1, 0.3), 2)}'),
+            ],
+          ),
         ),
       ),
     );
